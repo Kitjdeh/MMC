c5a587902ba24c80f736ab292b10706251861f05
김형균 <heng2926@naver.com>
2023-01-21 22:50:50 +0900

    🧱 BUILD : spring boot init

---
+++ mmc/src/main/java/com/mmt/mmc/MmcApplication.java
@@ -0,0 +1,13 @@
+package com.mmt.mmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmcApplication.class, args);
+	}
+
+}
