e92abe3a84f81a28fc1d052817fcb74394361e2e
김정민 <kjm3495@naver.com>
2023-02-14 21:33:54 +0900

    ⭐ FEAT : 강의노트 시작 모달 및 알람 추가

--- front/src/component/LectureWebRTC.js
+++ front/src/component/LectureWebRTC.js
@@ -1,132 +1,91 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("localhost:8001");
-const enterCode = "12345";
+const socket = io("localhost:8001", { transports: ["websocket"] });
 
-const handleIce = (data) => {
-  console.log("sent candidate");
-  socket.emit("ice", data.candidate, enterCode);
-};
+//const socket = io.connect({ hostname: "127.0.0.1", port: 8001 });
 
-let peerStream;
+const enterCode = "2";
 
-var myStream;
-let isScreen = 0;
-
-let myDataChannel;
-var myPeerConnection = new RTCPeerConnection({
-  iceServers: [
-    {
-      urls: [
-        "stun:stun.l.google.com:19302",
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun3.l.google.com:19302",
-        "stun:stun4.l.google.com:19302",
-      ],
-    },
-  ],
-});
-
-const handleAddStream = (data) => {
-  peerStream = data.stream;
-};
-
-const makeConnection = () => {
-  navigator.mediaDevices
-    .getUserMedia({
-      audio: false,
-      video: true,
-    })
-    .then((stream) => {
-      myStream = stream;
-      console.log(myStream);
-      myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
-
-      myPeerConnection.addEventListener("icecandidate", handleIce);
-      myPeerConnection.addEventListener("addstream", handleAddStream);
-
-      socket.emit("join_room", enterCode);
-    });
-};
-
-socket.on("connect", () => {
-  console.log("connected to server");
-  makeConnection();
-});
-
-socket.on("welcome", async () => {
-  const offer = await myPeerConnection.createOffer();
-  console.log("sent the offer");
-  myPeerConnection.setLocalDescription(offer);
-  socket.emit("offer", offer, enterCode);
-});
-
-socket.on("offer", async (offer) => {
-  myPeerConnection.addEventListener("datachannel", (event) => {
-    myDataChannel = event.channel;
-    console.log(myDataChannel);
-  });
-
-  console.log("received the offer");
-  myPeerConnection.setRemoteDescription(offer);
-  const answer = await myPeerConnection.createAnswer();
-  myPeerConnection.setLocalDescription(answer);
-  socket.emit("answer", answer, enterCode);
-  console.log("sent the answer");
-});
-
-socket.on("answer", (answer) => {
-  myPeerConnection.setRemoteDescription(answer);
-  console.log("received the answer");
-});
-
-socket.on("ice", (ice) => {
-  console.log("caught candidate");
-  myPeerConnection.addIceCandidate(ice);
-});
-
-socket.on("setScreen", (num) => {
-  isScreen = num;
-  console.log(isScreen);
-});
-
-const LectureWebRTC = ({ lectureNoteId, check, setCheck }) => {
-  useEffect(() => {
-    if (check !== 4) {
-      setCheck(4);
-    }
-  }, []);
+const LectureWebRTC = ({
+  myStream,
+  peerStream,
+  myPeerConnection,
+  isScreen,
+  setScreen,
+  check,
+  setCheck,
+}) => {
+  console.log(peerStream);
+  console.log("aaa");
+  console.log(myPeerConnection);
 
   const myFace = useRef();
   const muteBtn = useRef();
   const cameraBtn = useRef();
   const peersFace = useRef();
   const screenBtn = useRef();
+
+  const [screenCtr, setScreenCtr] = useState(isScreen);
+
   let muted = true;
+
   let cameraOff = false;
+  let peerPlaying = false;
+  let mePlaying = false;
 
   let peerData = peerStream;
 
-  const handleAddStream = (data) => {
-    getPeersMedia(data.stream);
+  peerData.onaddtrack = function () {
+    peersFace.srcObject = peerData;
   };
-  myPeerConnection.addEventListener("addstream", handleAddStream);
+
+  peerData.onremovetrack = function () {
+    peersFace.srcObject = myStream;
+    myFace.srcObject = null;
+  };
+
+  socket.on("goodbye", () => {
+    peersFace.current.srcObject = null;
+  });
 
   const getMedia = () => {
     let video = myFace.current;
-
+    if (mePlaying) {
+      mePlaying = false;
+      video.pause();
+    }
     video.srcObject = myStream;
 
-    video.play();
+    video
+      .play()
+      .then(() => {
+        mePlaying = true;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const getPeersMedia = (data) => {
+  const getPeersMedia = () => {
     let video = peersFace.current;
-    video.srcObject = data;
-    console.log(data);
-    video.play();
+    if (peerPlaying) {
+      video.pause();
+      peerPlaying = false;
+    }
+    video.srcObject = peerData;
+    console.log(video.srcObject);
+    console.log(peerData);
+
+    video
+      .play()
+      .then(() => {
+        peerPlaying = true;
+        console.log("alreaady playing");
+      })
+      .catch((error) => {
+        console.log("no video yet");
+      });
   };
 
   const handleMuteClick = () => {
@@ -167,54 +126,73 @@ const LectureWebRTC = ({ lectureNoteId, check, setCheck }) => {
   };
 
   const handleScreenClick = () => {
-    if (isScreen) return;
+    console.log(screenCtr);
+    if (screenCtr) return;
     navigator.mediaDevices
       .getDisplayMedia({
         video: { cursor: "always" },
         audio: { echoCancellation: true, noiseSuppression: true },
       })
       .then((stream) => {
-        peersFace.current.srcObject = stream;
+        if (peerData) {
+          peersFace.current.srcObject = stream;
+          myFace.current.srcObject = peerData;
+        }
         const videoTrack = stream.getVideoTracks()[0];
+        console.log("send 1");
         socket.emit("setScreen", enterCode, 1);
-        isScreen = 1;
+        setScreenCtr(1);
+        setScreen(screenCtr);
+        // this.props.PeerControl(videoTrack);
+
         myPeerConnection
           .getSenders()
           .find((sender) => sender.track.kind === videoTrack.kind)
           .replaceTrack(videoTrack);
 
         videoTrack.onended = function () {
+          console.log("hihi");
           let screenTrack = myStream.getVideoTracks()[0];
-          isScreen = 0;
+          setScreenCtr(0);
+          setScreen(screenCtr);
           socket.emit("setScreen", enterCode, 0);
           myPeerConnection
             .getSenders()
             .find((sender) => sender.track.kind === screenTrack.kind)
             .replaceTrack(screenTrack);
           stream.getTracks().forEach((track) => track.stop());
-
-          peersFace.current.srcObject = peerStream;
+          console.log(peerData);
+          if (peerData) {
+            peersFace.current.srcObject = peerStream;
+            myFace.current.srcObject = myStream;
+          }
         };
       });
   };
+
+  useEffect(() => {
+    getPeersMedia();
+  }, [peersFace]);
 
   useEffect(() => {
     getMedia();
   }, [myFace]);
 
   useEffect(() => {
-    getPeersMedia(peerData);
-  }, [peersFace]);
+    if (check !== 4) {
+      setCheck(4);
+    }
+  }, []);
 
   return (
     <>
-      <button id="mute" onClick={handleMuteClick} ref={muteBtn}>
+      <button id="mute" type="button" onClick={handleMuteClick} ref={muteBtn}>
         Mute
       </button>
-      <button id="camera" onClick={handleCameraClick} ref={cameraBtn}>
+      <button id="camera" type="button" onClick={handleCameraClick} ref={cameraBtn}>
         Turn Camera Off
       </button>
-      <button id="shareScreen" onClick={handleScreenClick} ref={screenBtn}>
+      <button id="shareScreen" type="button" onClick={handleScreenClick} ref={screenBtn}>
         화면 공유
       </button>
 
--- front/src/page/LectureNote.js
+++ front/src/page/LectureNote.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
-import { Grid, Button, Box } from "@material-ui/core";
+import React, { useState, useMemo, useEffect } from "react";
+import { Grid, Button, Box, Modal } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import LectureQuestion from "../component/LectureQuestion";
 import LectureCode from "../component/LectureCode";
 import LectureGraffiti from "../component/LectureGraffiti";
 import LectureWebRTC from "../component/LectureWebRTC";
 import LectureChat from "../component/LectureChat";
+import Clock from "../component/Clock";
 import jsPDF from "jspdf";
 // import { useSelector } from "react-redux";
+import io from "socket.io-client";
+
+//webRTC setting
+const socketRTC = io("localhost:8001", { transports: ["websocket"] });
 
 const useStyles = makeStyles({
   bar: {
@@ -17,8 +22,20 @@ const useStyles = makeStyles({
   word: {
     textAlign: "center",
   },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: "white",
+    border: "2px solid #000",
+    boxShadow: 24,
+    padding: 16,
+  },
 });
 
+// WebSocket
 const socket = new WebSocket(`ws://localhost:8000`);
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
@@ -29,6 +46,8 @@ socket.addEventListener("close", () => {
 });
 
 const LectureNote = () => {
+  const [start, setStart] = useState(false); // 둘다 들어올시 시작
+  const [openModal, setOpenModal] = useState(false); // 준비 버튼 누를시 모달창 뜸
   // const nickName = useSelector((state) => state.userinfo.userinfo).nickName;
   // const lectureNoteId = useSelector((state) => state.note.note);
   const lectureNoteId = 2;
@@ -36,6 +55,24 @@ const LectureNote = () => {
   const classes = useStyles();
   const [content, setContent] = useState("Question");
   const [check, setCheck] = useState(0);
+  const [peerStream, setPeerStream] = useState();
+  const [isScreen, setScreen] = useState(0);
+  const [myStream, setMyStream] = useState();
+  const [myPeerConnection] = useState(
+    new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302",
+          ],
+        },
+      ],
+    })
+  );
   const [pdfimg, setPdfimg] = useState({
     Question: "",
     Graffiti: "",
@@ -44,7 +81,103 @@ const LectureNote = () => {
   img.src =
     "https://ssafy-mmc.s3.ap-northeast-2.amazonaws.com/13793120a9ba4c50a4e2bd9f390abf1f.jpg";
 
+  // webRTC
+  const enterCode = "123";
+  const handleAddStream = (data) => {
+    setPeerStream(data.stream);
+  };
+  const handleIce = (data) => {
+    console.log("sent candidate");
+    socketRTC.emit("ice", data.candidate, lectureNoteId);
+  };
+
+  const handleOpen = () => {
+    socketRTC.emit("join_room", lectureNoteId);
+    socketRTC.emit("modal", enterCode);
+    setOpenModal(true);
+  };
+
+  socketRTC.on("settingTime", (obj) => {
+    setStart(true);
+
+    socketRTC.emit("setTime", enterCode);
+  });
+
+  socketRTC.on("setTime", (obj) => {
+    setStart(true);
+  });
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: true,
+      })
+      .then((stream) => {
+        //
+        setMyStream(stream);
+
+        stream.getTracks().forEach((track) => myPeerConnection.addTrack(track, stream));
+      });
+
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+
+    socketRTC.on("connect", () => {
+      console.log("connected to server");
+    });
+
+    socketRTC.on("welcome", async () => {
+      const offer = await myPeerConnection.createOffer();
+      console.log("sent the offer");
+      myPeerConnection.setLocalDescription(offer);
+      socketRTC.emit("offer", offer, lectureNoteId);
+    });
+
+    socketRTC.on("offer", async (offer) => {
+      console.log("received the offer");
+      console.log(offer);
+      myPeerConnection.setRemoteDescription(offer);
+      const answer = await myPeerConnection.createAnswer();
+      myPeerConnection.setLocalDescription(answer);
+      socketRTC.emit("answer", answer, lectureNoteId);
+      console.log("sent the answer");
+    });
+
+    socketRTC.on("answer", (answer) => {
+      console.log(answer);
+      myPeerConnection.setRemoteDescription(answer);
+      console.log("received the answer");
+    });
+
+    socketRTC.on("ice", (ice) => {
+      console.log("caught candidate");
+      console.log(ice);
+      myPeerConnection.addIceCandidate(ice);
+    });
+
+    socketRTC.on("setScreen", (num) => {
+      setScreen(num);
+      console.log(num);
+    });
+
+    socketRTC.on("goodbye", () => {
+      // myPeerConnection.getSenders().map((sender) => sender.replaceTrack(null));
+      // console.log(myPeerConnection.getSenders().map((sender) => sender.track));
+
+      // setPeerStream(null);
+      console.log("byebye");
+    });
+
+    return () => {
+      socketRTC.off("connect");
+      myPeerConnection.removeEventListener("icecandidate", handleIce);
+      myPeerConnection.removeEventListener("addstream", handleAddStream);
+    };
+  }, []);
+
   const handleDownload = async () => {
+    console.log(openModal);
     const pdf = new jsPDF();
     // await new Promise((resolve) => {
     //   img.onload = resolve;
@@ -57,68 +190,100 @@ const LectureNote = () => {
     pdf.save("canvas-image.pdf");
   };
 
-  const category = {
-    WebRTC: <LectureWebRTC lectureNoteId={lectureNoteId} check={check} setCheck={setCheck} />,
-    Question: (
-      <LectureQuestion
-        lectureNoteId={lectureNoteId}
-        check={check}
-        setCheck={setCheck}
-        img={img}
-        pdfimg={pdfimg}
-        socket={socket}
-      />
-    ),
-    Code: <LectureCode check={check} setCheck={setCheck} />,
-    Graffiti: (
-      <LectureGraffiti
-        lectureNoteId={lectureNoteId}
-        check={check}
-        setCheck={setCheck}
-        img={img}
-        pdfimg={pdfimg}
-        socket={socket}
-      />
-    ),
-    Chat: <LectureChat nickName={nickName} lectureNoteId={lectureNoteId} socket={socket} />,
-  };
+  const category = useMemo(
+    () => ({
+      WebRTC: (
+        <LectureWebRTC
+          myStream={myStream}
+          peerStream={peerStream}
+          myPeerConnection={myPeerConnection}
+          isScreen={isScreen}
+          setScreen={setScreen}
+          check={check}
+          setCheck={setCheck}
+        />
+      ),
+      Question: (
+        <LectureQuestion
+          lectureNoteId={lectureNoteId}
+          check={check}
+          setCheck={setCheck}
+          img={img}
+          pdfimg={pdfimg}
+          socket={socket}
+        />
+      ),
+      Code: <LectureCode check={check} setCheck={setCheck} />,
+      Graffiti: (
+        <LectureGraffiti
+          lectureNoteId={lectureNoteId}
+          check={check}
+          setCheck={setCheck}
+          img={img}
+          pdfimg={pdfimg}
+          socket={socket}
+        />
+      ),
+      Chat: <LectureChat nickName={nickName} lectureNoteId={lectureNoteId} socket={socket} />,
+      Clock: <Clock lectureNoteId={lectureNoteId} socket={socket} />,
+    }),
+    [myStream, peerStream, myPeerConnection]
+  );
 
   return (
     <Grid container justify="space-between">
-      <Grid item xs={1} className={classes.bar}>
-        Buttons
-        <Grid container direction="column" alignItems="flex-start" spacing={2}>
-          <Grid item xs={4} className={classes.word}>
-            <Button onClick={() => setContent("Question")} variant="contained" color="primary">
-              Question
-            </Button>
+      {!start ? (
+        <>
+          <Button variant="contained" color="primary" onClick={handleOpen}>
+            준비가 됫으면 눌러주세요!!!!
+          </Button>
+          <Modal open={openModal} className={classes.modal}>
+            <Box className={classes.paper}>
+              <h2>대기중</h2>
+              <p>상대방이 아직 입장하지 않았습니다!!</p>
+            </Box>
+          </Modal>
+        </>
+      ) : (
+        <>
+          <Grid item xs={1} className={classes.bar}>
+            Buttons
+            <Grid container direction="column" alignItems="flex-start" spacing={2}>
+              <Grid item xs={4} className={classes.word}>
+                <Button onClick={() => setContent("Question")} variant="contained" color="primary">
+                  Question
+                </Button>
+              </Grid>
+              <Grid item xs={4} className={classes.word}>
+                <Button onClick={() => setContent("Code")} variant="contained" color="primary">
+                  Code
+                </Button>
+              </Grid>
+              <Grid item xs={4} className={classes.word}>
+                <Button onClick={() => setContent("Graffiti")} variant="contained" color="primary">
+                  Graffiti
+                </Button>
+              </Grid>
+              <Grid item xs={4} className={classes.word}>
+                <Button onClick={() => setContent("WebRTC")} variant="contained" color="primary">
+                  WebRTC
+                </Button>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={4} className={classes.word}>
-            <Button onClick={() => setContent("Code")} variant="contained" color="primary">
-              Code
-            </Button>
+          <Grid item xs={9} className={classes.bar}>
+            Screen
+            <Box>{category["Clock"]}</Box>
+            {<Box>{category[content]}</Box>}
           </Grid>
-          <Grid item xs={4} className={classes.word}>
-            <Button onClick={() => setContent("Graffiti")} variant="contained" color="primary">
-              Graffiti
-            </Button>
+          <Grid item xs={2} className={classes.bar}>
+            Chat Window
+            <Box>{category["Chat"]}</Box>
           </Grid>
-          <Grid item xs={4} className={classes.word}>
-            <Button onClick={() => setContent("WebRTC")} variant="contained" color="primary">
-              WebRTC
-            </Button>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={9} className={classes.bar}>
-        Screen
-        {<Box>{category[content]}</Box>}
-      </Grid>
-      <Grid item xs={2} className={classes.bar}>
-        Chat Window
-        <Box>{category["Chat"]}</Box>
-      </Grid>
-      <button onClick={handleDownload}>Download</button>
+          <button onClick={handleDownload}>Download</button>
+        </>
+      )}
+      ;
     </Grid>
   );
 };
