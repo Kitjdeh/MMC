f654f8c268830517d139559fb89aa56307cc8088
김원혁 <wonhyuck@korea.ac.kr>
2023-02-14 13:22:49 +0900

    📝 UPDATE : webRTC 서버 수정

--- WebRTC/src/server.js
+++ WebRTC/src/server.js
@@ -9,14 +9,49 @@ const server = http.createServer(app);
 
 const wsServer = io(server, { cors: { origin: "*" } });
 const port = 8001;
+const status = {
+  123: {
+    hour: 0,
+    min: 0,
+    sec: 0,
+    ttlTime: 0,
+    change: 0,
+  },
+};
 
-wsServer.listen(port);
+const roomNumber = "123";
+
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+
+  return publicRooms;
+  // const sids = wsServer.sockets.adapter.sids;
+  // const rooms = wsServer.sockets.adapter.rooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+const handleListen = () => console.log(`Listening on localhost:${port}`);
+
+wsServer.listen(port, handleListen);
 
 wsServer.on("connection", (socket) => {
   console.log("connect client by Socket.io");
 
   socket.on("join_room", (roomName) => {
     socket.join(roomName);
+    console.log(roomName);
     socket.to(roomName).emit("welcome");
   });
   socket.on("offer", (offer, roomName) => {
@@ -33,5 +68,62 @@ wsServer.on("connection", (socket) => {
 
   socket.on("setScreen", (roomName, num) => {
     socket.to(roomName).emit("setScreen", num);
+  });
+
+  // 시간 설정
+  socket.on("timerStart", (roomName) => {
+    socket.join(roomName);
+
+    wsServer.sockets.emit("room_change", publicRooms());
+    //console.log(wsServer.sockets.adapter);
+
+    console.log(countRoom(roomName));
+    if (!status[roomName]) {
+      status[roomName] = {
+        hour: 0,
+        min: 0,
+        sec: 0,
+        ttlTime: 0,
+        change: 0,
+      };
+    }
+
+    if (countRoom(roomName) === 2) {
+      let time = new Date();
+      status[roomName].hour = time.getHours();
+      status[roomName].min = time.getMinutes();
+      status[roomName].sec = time.getSeconds();
+    }
+
+    status[roomName].change = countRoom(roomName);
+    //console.log(status[roomName]);
+    socket.to(roomName).emit("settingTime", status[roomName]);
+  });
+
+  socket.on("setTime", (roomName) => {
+    socket.to(roomName).emit("setTime", status[roomName]);
+  });
+
+  socket.on("disconnect", (data) => {
+    //console.log(wsServer.sockets.adapter);
+    const roomNum = roomNumber;
+    console.log("byebye");
+    socket.to("12345").emit("goodbye");
+    socket.to(roomNum).emit("stopTime");
+    let time = new Date();
+    console.log(status[roomNum]);
+    if (countRoom(roomNum) !== status[roomNum].change) {
+      status[roomNum].ttlTime +=
+        time.getHours() * 3600 +
+        time.getMinutes() * 60 +
+        time.getSeconds() -
+        (status[roomNum].hour * 3600 + status[roomNum].min * 60 + status[roomNum].sec);
+    }
+
+    console.log(status[roomNum]);
+
+    status[roomNum].change = countRoom(roomNum);
+
+    wsServer.sockets.emit("room_change", publicRooms());
   });
 });
