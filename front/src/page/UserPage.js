d9960485f196973ae2ca3278a4f9a341f36dc5b8
Kitjdeh <rltjdeh@naver.com>
2023-02-15 09:16:40 +0900

    ⭐ FEAT : 네브바,main 페이지 수정

--- front/src/page/UserPage.js
+++ front/src/page/UserPage.js
@@ -13,9 +13,9 @@ import { Routes, Route } from "react-router-dom";
 import AllQuestion from "./AllQuestion";
 import Main from "./Main";
 import MyLecture from "./MyLecture";
-import Admin from './Admin';
-import { Cookies } from 'react-cookie';
-
+import Admin from "./Admin";
+import { Cookies } from "react-cookie";
+import MainBackground from "../component/MainBackground";
 const theme = createTheme({
   palette: {
     mode: "dark",
@@ -25,26 +25,29 @@ const theme = createTheme({
   },
 });
 const UserPage = () => {
-  const cookie=new Cookies();
-  const userId=cookie.get("userId");
+  const cookie = new Cookies();
+  const userId = cookie.get("userId");
   return (
     <div>
       <Navbar />
-      <Box className="body">
-        <Routes>
+        <Box className="body">
+      <Routes>
           {" "}
-          <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/question" element={<AllQuestion />} />
           <Route path="/question/:id" element={<QuestionDetail />} />
-          <Route path="/question/write" element={<WriteQuestion userId={userId} />} />
+          <Route
+            path="/question/write"
+            element={<WriteQuestion userId={userId} />}
+          />
           <Route path="/mypage/point" element={<Point />} />
           <Route path="/mypage/question" element={<MyQuestion />} />
           <Route path="/mypage/lecture" element={<MyLecture />} />
           <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Box>
+          <Route path="/" element={<MainBackground />} />
+      </Routes>
+        </Box>
     </div>
   );
 };
