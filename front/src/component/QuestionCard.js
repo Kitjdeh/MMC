85b449b30d1d64f9f6d02ffa85e701e00c97a722
김형균 <heng2926@naver.com>
2023-02-16 17:39:38 +0900

    📝 UPDATE : 질문카드 채택 기준으로 색 변경 , 나의 강의 페이지 수정

--- front/src/component/QuestionCard.js
+++ front/src/component/QuestionCard.js
@@ -19,15 +19,26 @@ const Item = styled(Paper)(({ theme }) => ({
   minWidth: 60,
   maxWidth: 400,
 }));
-
+const DoneItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#E8DAC3",
+  padding: theme.spacing(0.3),
+  textAlign: "center",
+  minWidth: 60,
+  maxWidth: 400,
+}));
 const QuestionCard = ({ question }) => {
+  console.log(question,"111111111111")
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const showDetail = () => {
     navigate(`/question/${question.questionId}`);
   };
   return (
-    <Item
+   <div>
+
+   
+     { question?.progress > 0 ? (
+      <DoneItem
       sx={{
         p: 2,
         margin: "50",
@@ -35,12 +46,12 @@ const QuestionCard = ({ question }) => {
         height: 250
       }}
       onClick={showDetail}
-    >
+      >
       <Box component="form" noValidate sx={{ color: "#6C5D53", mt: 1, alignItems: "center" }}>
         테마
       </Box>
 
-      <Box sx={{ color: "#6C5D53"}}>
+<Box sx={{ color: "#6C5D53"}}>
         <TypographyOtf sx={{ color: "#6C5D53"}} variant="h6" align="right">
           {question.title}
         </TypographyOtf>
@@ -73,8 +84,61 @@ const QuestionCard = ({ question }) => {
           </Grid>
         </Grid>
       </Box>
-    </Item>
-  );
-};
+    </DoneItem>
+     )
+  : 
+  (
 
+    <Item
+    sx={{
+      p: 2,
+      margin: "50",
+      width: 250,
+      height: 250
+    }}
+    onClick={showDetail}
+    >
+    <Box component="form" noValidate sx={{ color: "#6C5D53", mt: 1, alignItems: "center" }}>
+      테마
+    </Box>
+
+<Box sx={{ color: "#6C5D53"}}>
+      <TypographyOtf sx={{ color: "#6C5D53"}} variant="h6" align="right">
+        {question.title}
+      </TypographyOtf>
+    </Box>
+    <Box>
+      <Grid sx={{ color: "#6C5D53"}} container spacing={1}>
+        <Grid item xs={8} textAlign="left"></Grid>
+        <Grid item xs={4} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs container alignItems="center">
+              <TypographyOtf variant="h8" align="right">
+                {new Date(question.reservation).toLocaleString("ko-kr", {
+                  month: "short",
+                  day: "2-digit",
+                  year: "numeric",
+                })}
+              </TypographyOtf>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={8} textAlign="left">
+          {question.userId}
+        </Grid>
+        <Grid item xs={4} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs container alignItems="center">
+              <TypographyOtf sx={{ color: "#6C5D53"}} variant="body1">{question.point}point</TypographyOtf>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  </Item>
+  )  }
+  </div>
+   )
+;
+              }
 export default QuestionCard;
--- front/src/page/MyLecture.js
+++ front/src/page/MyLecture.js
@@ -7,12 +7,22 @@ import { mypageAction } from "../redux/actions/mypageAction";
 import { useSelector } from 'react-redux';
 import { Cookies } from 'react-cookie';
 import { styled } from "@mui/material/styles";
+import { Box } from '@material-ui/core';
+import { fontSize, margin } from '@mui/system';
 const Mygrid = styled(Grid)(({ theme }) => ({
-  backgroundColor: "#f6edff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
+  // backgroundColor: "#FFBEBE",
+  // ...theme.typography.body2,
+  // padding: theme.spacing(1),
+  // textAlign: "center",
+  fontFamily: "BMHANNAProOTF",
+}));
+const Title = styled(Box)(()=> ({
+  margin: "50px"
+}));
+const Myspan = styled(Box)(() => ({
+  fontFamily: "BMHANNAProOTF",
+  color: "#917B56",
+  padding: "5px 5px 5px 55px",
 }));
 const MyLecture = () => {
   useEffect(() => {
@@ -29,15 +39,18 @@ const MyLecture = () => {
   return (
    lecturesList.length > 0 ? (
       <Container>
+        <Title><h1>나의 강의</h1></Title>
         <Grid container spacing={1}>
         {lecturesList.map((lecture) =>
           lecture.progress == userId ? (
             <Mygrid item xl={3} lg={4} md={6}>
-              채택된 강의
+              <Myspan> 🔓 채택 완료 강의 🔓 </Myspan>
               <QuestionCard question={lecture} />
             </Mygrid>
           ) : (
-            <Grid item xl={3} lg={4} md={6}>
+            // <Grid item xl={3} lg={4} md={6} sx={{color: "#917B56" , textAlign: "center"}}>
+            <Grid item xl={3} lg={4} md={6} sx={{color: "#917B56"}}>
+              <Myspan> 🔒 채택 미완료 강의 🔒 </Myspan>
               <QuestionCard question={lecture} />
               <br />
             </Grid>
